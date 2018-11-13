e64b877275f8d7f914ff24564653d751363209c0
Heromyth <bitworld@qq.com>
2018-11-13 12:30:23 +0800

    Testing JSON converter

--- website-basic/source/app/controller/GreetingController.d
+++ website-basic/source/app/controller/GreetingController.d
@@ -21,26 +21,65 @@ class GreetingController : WebSocketController {
 
         return "Hello " ~ message ~ "! CreationTime: " ~ creationTime.toString() ~ 
             " CurrentTime:" ~ Clock.currTime.toString();
-        // Thread.sleep(1000); // simulated delay
         // return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + 
         // "! CreationTime: " + creationTime + " CurrentTime:" + System.currentTimeMillis());
     }
 
     import hunt.framework.messaging.Message;
+    import hunt.framework.messaging.converter.AbstractMessageConverter;
+    import hunt.framework.messaging.converter.MessageConverter;
+    import hunt.framework.messaging.converter.MessageConverterHelper;
+    import hunt.http.codec.http.model.MimeTypes;
     import hunt.lang.Nullable;
-
+    import hunt.logging;
+    import std.json;
+    
     shared static this() {
         WebSocketControllerHelper.registerController!GreetingController();
     }
 
     
     override protected void __invoke(string methodName, MessageBase message, ReturnHandler handler ) {
+
         version(HUNT_DEBUG) info("invoking: ", methodName);
+        
+        MessageConverter messageConverter = 
+            annotationHandler.getMessageConverter();
+
+        // MimeType mt = messageConverter.getMimeType();
+        // info(mt.toString());
+            
+        Object ob = messageConverter.fromMessage(message, typeid(JSONValue));
+        if(ob is null)
+            warning("no payload");
+        else {
+             version(HUNT_DEBUG) infof("playload: %s", typeid(ob));
+        }
+
         switch(methodName) {
             case "greeting" : {
-                string r = greeting("message");
-                if(handler !is null) 
-                    handler(new Nullable!string(r), typeid(string));
+                // string str = MessageConverterHelper.fromMessage!string(message);
+                // auto temp = cast(Nullable!string) ob;
+                auto temp = cast(Nullable!JSONValue) ob;
+                if(temp is null) {
+                    warningf("Wrong pyaload type: %s", typeid(ob));
+                    return;
+                }
+                JSONValue jv = temp.value;
+                string str = jv["name"].str;
+                tracef("incoming message: %s", str);
+                string r = greeting(str);
+                tracef("outgoing message: %s", r);
+                // messageConverter.toMessage(message, typeid(string));
+
+                if(handler !is null) {
+
+                    handler(new Nullable!string(r), typeid(string));  
+                }          
+
+                // string r = greeting(str);
+                // if(handler !is null) 
+                //     handler(new Nullable!string(r), typeid(string));
                 break;
             }
 
